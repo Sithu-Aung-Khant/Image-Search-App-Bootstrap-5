2911c791c5de45bb5df9baa63c0d3ba9563ffd75
Sithu Aung Khant <sithu.sithuthedev@gmail.com>
2023-09-28 11:55:01 +0630

    Image-Search-App-Recreation-with-Bootstrap-5

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image Search App rebuilding with Bootstrap
+
+I've followed buildng a JS Project | Image Search App with Pexels API [VIDEO](https://www.youtube.com/watch?v=oaliV2Dp7WQ&t=447s&ab_channel=Tech2etc)
+
+I try rebuild a image search app using Bootstrap and here is what I learned. I tried to replace all CSS codes with Bootstrap and I faced a lot of challenges and learned a lot from it.
+
+The real pain was when I try to manipulat DOM with event listensers. In Plain CSS version, I just had to create an element(div, a, image.etc), add a class and that's it. But when I try to do the same thing with Bootstrap, I have to add so many classes because as you know bootstrap is used by adding classes in an HTML element. I build all the grid system, marging, display all that stuffs in html elements so when I tried to manipulate from the resuls of calling and API, I have to readd all the bootstraps classes within the function. Here is the Screenshots of these two for better understanding.
+
+#### DOM MANIPULATION WITH CSS
+![](./assets/code.png)
+
+#### DOM Manipulation With Bootstrap
+![](./assets/code.with.bootstrap.png)
+
+the main thing
---
+++ script.js
@@ -0,0 +1,64 @@
+const accessKey = "yQzePyAUeZm9BCbkabSBHdXQRFvoNPOzEL75wWMdb8Y";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = '';
+let page = 1;
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if (page === 1) {
+        searchResults.innerHTML = "";
+    }
+    results.map((result) => {
+        const imgWrapper = document.createElement('div');
+        imgWrapper.classList.add('search-result', 'card', 'col-md-5', 'col-lg-3', 'p-0', 'm-3', 'shadow');
+        const imgLink = document.createElement('a');
+        imgLink.href = result.links.html;
+        imgLink.target = '_blank';
+        imgLink.rel = 'noopener';
+        imgLink.classList.add('text-decoration-none', 'text-dark');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        image.classList.add('card-img-top', 'img-fluid');
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        const cardText = document.createElement('p');
+        cardText.classList.add('card-text', 'text-center', 'fs-5', 'text-capitalize');
+        cardText.textContent = result.alt_description;
+
+        imgWrapper.appendChild(imgLink); 
+        imgLink.appendChild(image); 
+        imgLink.appendChild(cardBody); 
+        cardBody.appendChild(cardText); 
+
+        searchResults.appendChild(imgWrapper);
+    });
+
+    page++;
+    if(page > 1) {
+        showMore.classList.remove('d-none');
+    }
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    searchImages();
+});
